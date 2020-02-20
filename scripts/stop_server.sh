21ba5acaa7c2810a796db744b6eeb07db0a57c09
singhlovepreet9 <lovepreet.singh61182@gmail.com>
2020-02-20 14:20:52 +0530

    testing

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,3 +1,7 @@
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
+source /home/ubuntu
 pm2 kill all
 pm2 delete all
