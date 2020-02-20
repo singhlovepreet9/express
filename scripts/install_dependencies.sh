91052f29b36956256fc846d8894b3a8f9c8554d1
singhlovepreet9 <lovepreet.singh61182@gmail.com>
2020-02-20 14:14:41 +0530

    testing

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,3 @@
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 cd /home/ubuntu/express
 rm -rf node_modules
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,5 +1,3 @@
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 pm2 kill all
 pm2 delete all
