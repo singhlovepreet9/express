6bf66f2b5e29a0a37400daa619c5fe604d82708c
singhlovepreet9 <lovepreet.singh61182@gmail.com>
2020-02-20 13:50:23 +0530

    testing changes

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const express=require('express');
+const express = require('express');
 const app = express();
-app.set('view engine','pug');
-app.set('views','./views');
+app.set('view engine', 'pug');
+app.set('views', './views');
 
-
-app.get('/hello',(req,res)=>{
-    console.log('dmfbhdbfj')
-    res.render('index',{title:"My app",message:'hello world'});
+app.get('/hello', (req, res) => {
+	console.log('dmfbhdbfj');
+	res.render('index', { title: 'My appv2', message: 'hello worldv2' });
 });
 
+const port = process.env.PORT || 4000;
 
-const port=process.env.PORT||4000;
-
-app.listen(port,()=>console.log(`listening to port ${port}....`))
+app.listen(port, () => console.log(`listening to port ${port}....`));
