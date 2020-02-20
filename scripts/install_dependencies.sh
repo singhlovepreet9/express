9d7f225e3b408a1e560021e5fa00d4f27a15f540
singhlovepreet9 <lovepreet.singh61182@gmail.com>
2020-02-20 14:39:54 +0530

    testing

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,5 @@
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
+source ~/.bashrc
 cd /home/ubuntu/express
 rm -rf node_modules
 npm install
